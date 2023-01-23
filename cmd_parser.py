53159f2f38c72e273447e1d833918902a8c93c95
ben.richardson <richardson@is.mpg.de>
2023-01-23 20:29:12 +0100

    add and remove some params

--- cmd_parser.py
+++ cmd_parser.py
@@ -7,9 +7,6 @@ Created on Thu Jan  5 18:59:27 2023
 """
 
 import configargparse
-import yaml
-import ast
-import pdb
 
 def parse_vae_config(argv=None):
     arg_formatter = configargparse.ArgumentDefaultsHelpFormatter
@@ -22,16 +19,15 @@ def parse_vae_config(argv=None):
     parser.add_argument('-c', '--config', required=True,
                         is_config_file=True,
                         help='config file')
-    parser.add_argument('--cuda',type=bool, default=False)
-
+    
+    parser.add_argument('--device',type=str, default='gpu',choices=['gpu','cpu'])
     # path parameters
     parser.add_argument('--data_path', type=str)
     # paths to save models
     parser.add_argument('--save_path', type=str, default=None)
-    parser.add_argument('--encoder_save', type=str, default='encoder', help="model save for encoder")
-    parser.add_argument('--decoder_save', type=str, default='decoder', help="model save for decoder")    
 
-    parser.add_argument('--load_saved', type=str, default=None, help="path to a saved model that will be loaded")
+    parser.add_argument('--load_checkpoint', type=str, default=None, help="path to run folder \
+                        containing checkpoint that will be loaded")
     parser.add_argument('--start_epoch', type=int, default=0, help="flag to set the starting epoch for training")
     parser.add_argument('--end_epoch', type=int, default=1000, help="flag to indicate the final epoch of training")
     parser.add_argument('--batch_size', type=int, default=32, help="batch size for training")
@@ -43,7 +39,7 @@ def parse_vae_config(argv=None):
  
     # architecture
     parser.add_argument('--style_dim', type=int, default=10, help="dimension of varying factor latent space")
-    parser.add_argument('--class_dim', type=int, default=10, help="dimension of common factor latent space")
+    parser.add_argument('--content_dim', type=int, default=10, help="dimension of common factor latent space")
     parser.add_argument('--in_channels', type=int, default=195)
     parser.add_argument('--hidden_dims', type=int, nargs='*', default=[128,64,64,32])
     parser.add_argument('--kernels', type=int, nargs='*', default=[6,6,5,4])
@@ -51,7 +47,7 @@ def parse_vae_config(argv=None):
     parser.add_argument('--paddings', type=int, nargs='*', default=[1,1,1,0])
 
     # loss
-    parser.add_argument('--class_coef', type=float, default=1., help="coefficient for class KL")
+    parser.add_argument('--content_coef', type=float, default=1., help="coefficient for class KL")
     parser.add_argument('--style_coef', type=float, default=1., help="coefficient for style KL")
     parser.add_argument('--beta_VAE',type=float, default=.1, help="beta coefficient for beta-VAE")
     parser.add_argument('--reduction', type=str, default='mean')
@@ -60,8 +56,9 @@ def parse_vae_config(argv=None):
   
     # dataset type
     parser.add_argument('--dataset', type=str, default='all', choices=['all','objects','new'])
+    parser.add_argument('--action_repetitions', type = int, default = 1)
+    parser.add_argument('--split_ratio',type=int,default=4,help="1/x: What fraction of data should be reserved for validation.")
 
     args = parser.parse_args(argv)
-    args_dict = vars(args)
 
-    return args_dict
+    return args
