662eb9268046a2cbdbd29452e5c4011d34101527
ben.richardson <richardson@is.mpg.de>
2023-02-01 21:03:31 +0100

    New run code bash scripts

--- run_inference.sh
+++ run_inference.sh
@@ -1,7 +1,7 @@
 SUBMIT_PATH="submit_utils"
+if [ -n "$1" ]; then REPEATS=$1; else REPEATS=1; fi
 
-
-
+WEIGHT_DECAY_ARRAY=(0.1)
 
 rm -f tmp_logs/*
 rm -f $SUBMIT_PATH/inf_args.txt
@@ -9,34 +9,41 @@ rm -f $SUBMIT_PATH/inf_args.txt
 
 function populate_template {
 export VAE_MODEL_PATH=$1
-export SAVE_PATH=""
-export DATA_REPETITIONS="1"
-export PROPERTIES_TO_TEST=""
+export SAVE_PATH="run$2"
+export DATA_REPETITIONS="5"
+export PROPERTIES_TO_TEST="" #"contents_binary_label"
 export ACTIONS_TO_USE=""
-export END_EPOCH="1"
-export WEIGHT_DECAY="1"
+export END_EPOCH="200"
+export WEIGHT_DECAY="$3"
 
-envsubst < $SUBMIT_PATH/infconfig.yaml > $1/inf_config.yaml
+mkdir -p $1/inference/run$2
+envsubst < $SUBMIT_PATH/infconfig.yaml > $1/inference/run$2/inf_config.yaml
 }
 
 function build_args_txt {
-echo "-c $1/inf_config.yaml">> $SUBMIT_PATH/inf_args.txt
+echo "-c $1/inference/run$2/inf_config.yaml">> $SUBMIT_PATH/inf_args.txt
 }
 
-for dir in runs/*/ ; do 
+CNT=1
+for dir in runs_all_obj/*/ ; do
     DIR=$(realpath -s $dir)
-    populate_template $DIR
-    build_args_txt $DIR
-    break 1
+    for rep in $(seq $REPEATS); do
+        for wgt in "${WEIGHT_DECAY_ARRAY[@]}"; do
+            populate_template $DIR $CNT $wgt
+            build_args_txt $DIR $CNT
+            CNT=$[CNT+1]
+        done
+    done
+    #break 1
     #echo "$dir"
 done
 
 
-if [ -n "$1" ]
+if [ -n "$2" ]
 then
-    condor_submit $1 "$SUBMIT_PATH"/inf_submit.sub
-else 
-    python inference_main.py -c $(realpath -s $DIR)/inf_config.yaml
+    condor_submit $2 "$SUBMIT_PATH"/inf_submit.sub
+else
+    python inference_main.py -c $(realpath -s $DIR)/inf_config.yaml --save_path=run$rep
 fi
 
 
@@ -44,7 +51,7 @@ fi
 
 
 
-
+#unstable_runs = 88,89,110,116,140,145,159
 
 
 
@@ -52,7 +59,7 @@ fi
 
 
 # function build_submission_file {
-# cat >$SUBMIT_PATH/inf_submit.sub <<EOL 
+# cat >$SUBMIT_PATH/inf_submit.sub <<EOL
 # executable = /home/richardson/miniconda3/envs/ball_env/bin/python
 # arguments = inference_main.py \$(a1)
 # error = /home/richardson/robot_haptic_perception/class_tmp_logs/lin-6-$(Process).err
---
+++ run_sgvae.sh
@@ -0,0 +1,79 @@
+UTILS_PATH="submit_utils"
+now=`date +"%Y-%m-%d:%S"`
+RUN_FOLDER="runs_$now"
+
+rm -f tmp_logs/*
+rm -f $UTILS_PATH/sgvae_args.txt
+
+if [ -n "$1" ]; then REPEATS=$1; else REPEATS=1; fi
+
+####  CREATE PARAMETER DICTIONARY   ####
+## -Make each parameter a key in the  ##
+## dictionary. Set as value or array. ##
+DATA_PATH_ARR="/fast/richardson/robot_grasp_data/"
+DATASET_ARR="new"
+declare -a ACTION_REPETITIONS_ARR=(1)
+declare -a STYLE_COEF_ARR=(0.1 0.5 1 5 10 15 20)
+declare -a KEEP_STYLE_ARR=(True False)
+declare -a UPDATE_PRIOR_ARR=(True False)
+declare -a BETA_VAE_ARR=(0.1 0.033 0.01 0.0033 0.001 0.00033 0.0001 0.000033 0.00001)
+declare -a param_names=(
+    "DATA_PATH_ARR" 
+    "DATASET_ARR" 
+    "ACTION_REPETITIONS_ARR"
+    "STYLE_COEF_ARR" 
+    "KEEP_STYLE_ARR" 
+    "UPDATE_PRIOR_ARR" 
+    "BETA_VAE_ARR"
+    )
+########################################
+### variables for tracking folder ID and current run config
+COUNTER=0
+declare -A RUN_PARAMS
+
+### function to populate the config file
+function populate_template {
+path=$RUN_FOLDER/$COUNTER
+export SAVE_PATH="$path"
+for key in ${!RUN_PARAMS[@]}; do
+    var_name="${key::-4}"
+    var_value="${RUN_PARAMS[${key}]}"
+    export $var_name=$var_value
+
+done
+
+mkdir -p $path
+envsubst < $UTILS_PATH/sgvae_config.yaml > $path/sgvae_config.yaml
+echo "-c $path/sgvae_config.yaml">> $UTILS_PATH/sgvae_args.txt
+COUNTER=$[COUNTER+1]
+exit 0
+}
+
+function recursive_parameters(){
+    local groups=("$@")
+    local group=${groups[0]}
+    local lst="$group[@]"
+    #echo "group name: ${group} with group members: ${!lst}"
+    for element in "${!lst}"; do
+        #echo -en "\tworking on $element of the $group group\n"    
+        RUN_PARAMS[$group]=$element
+        if (( ${#groups[@]} > 1 )); then
+            recursive_parameters ${groups[@]:1}
+        else
+            populate_template
+        fi
+    done
+}
+
+recursive_parameters "${param_names[@]}"
+
+if [ -n "$2" ]
+then
+    condor_submit $2 "$SUBMIT_PATH"/sgvae_submit.sub
+else
+    echo "doing nothing"
+    #python inference_main.py -c $(realpath -s $DIR)/inf_config.yaml --save_path=run$rep
+fi
+
+
+
